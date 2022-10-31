ccc2be8c8e6669a5a43ceb2c879a38e420f8c9c1
Murilll <murilosertorio25@gmail.com>
2022-10-31 14:39:30 -0300

    Commit

--- Classe_python.py
+++ Classe_python.py
@@ -1,1 +1,19 @@
-print("Hello, World")
+class Celular:
+    def __init__(self, tela, cor, android, memoriaram):
+        self.tela = tela
+        self.cor = cor
+        self.android = android
+        self.memoriaram = memoriaram
+        self.flash = False
+
+    def LigarFlash(self):
+        self.flash = self.flash
+
+celular1 = Celular("6.2pl","preto","12","8gb")
+celular2 = Celular("4.5pl","branco","10","2gb")
+celular3 = Celular("5pl","Azul","11","6gb")
+celular1.LigarFlash()
+
+print(celular1.tela)
+print(celular1.flash)
+
